c3f473850effb4eeca13c391aaf3ebe8364dc95c
Raj, Piyush <piyush.i.raj@accenture.com>
2025-07-28 22:11:03 +0530

    #004- changed config due to build failure

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-const { defineConfig } = require('vite')
 
 // https://vite.dev/config/
 export default defineConfig({
